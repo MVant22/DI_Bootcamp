e0d60b26ae720727f24ca1226457135c97ef8879
Major_ambiton0 <messonvantard@gmail.com>
2025-02-21 16:30:02 +0400

    Daily_challenge_actors

---
+++ Week6/Daily_challenge_Actors.sql
@@ -0,0 +1,26 @@
+-- Database: Hollywood
+
+-- DROP DATABASE IF EXISTS "Hollywood";
+
+-- CREATE DATABASE "Hollywood"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'English_United Kingdom.1252'
+--     LC_CTYPE = 'English_United Kingdom.1252'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+-- COMMENT ON DATABASE "Hollywood"
+--     IS 'First Database created';
+
+-- SELECT * FROM actors;
+
+-- SELECT COUNT(actor_id) FROM actors;
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars,type)
+-- VALUES ('Tom', '', '1990-12-12', 3, '')
+
+-- SELECT * FROM actors;
