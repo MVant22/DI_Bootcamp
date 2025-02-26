032f27ddb4416939194165e30859d9ab09028f55
Major_ambiton0 <messonvantard@gmail.com>
2025-02-26 23:55:45 +0400

    Exercise XP

---
+++ Week3/Day2/Exercise_XP.js
@@ -0,0 +1,150 @@
+// Exercise 1
+
+// let favouriteFood = 'noodles'
+// let meal = 'dinner'
+
+// console.log("i eat",favouriteFood, 'at every',meal);
+
+
+// Exercise 2 part 1
+
+// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+// myWatchedSeriesLength = myWatchedSeries.length;
+
+// let myWatchedSeriesSentence = "black mirror, money heist, and the big bang theory";
+
+// console.log("i watched",myWatchedSeriesLength, "series",myWatchedSeriesSentence)
+
+
+//Exercise 2 part 2
+// const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+// myWatchedSeries[2] = "friends";
+
+// myWatchedSeries.push = 'One piece';
+
+// myWatchedSeries.unshift("The witcher")
+
+// myWatchedSeries.splice(1,1)
+
+// console.log(myWatchedSeries[2])
+
+// console.log(myWatchedSeries)
+
+// Exercise 3
+// let tempCel = 23.5;
+
+// let tempfah = ((tempCel / 5)* 9) + 32;
+
+// console.log(tempCel,"C is", tempfah,"F");
+
+
+// Exercise 4
+
+// 1. Prediction: a=34 + b=21 = 55
+//    Actual: 55
+
+// 2. Prediction: a=2 + b=21 = 23
+//    Actual: 23
+
+// 3. Prediction: null
+//    Actual: undefined
+
+// 4. Prediction: int 3 + int 4 = 7 concatenate with string '5' = 75
+//    Actual: 75
+
+// Exercise 5
+// 1. Prediction: int
+//    Actual: number
+
+// 2. Prediction: float
+//    Actual: number
+
+// 3. Prediction: undefined
+//    Actual: number
+
+// 4. Prediction: string
+//    Actual: string
+
+// 5. Prediction: Boolean
+//    Actual: Booelan
+
+// 6. Prediction: Boolean
+//    Actual: Booelan
+
+// 7. Prediction: hamburger + s = hamburgers both variable are string datatype
+//    Actual: hamburgers
+
+// 8. Prediction: error 
+//    Actual: Nan
+
+// 9. Prediction: str 1 + str 3 = 13 
+//    Actual: 13
+
+// 10. Prediction: Nan
+//    Actual: -2
+
+// 11. Prediction: 'johnny' + 5 = johnny5 concatenate
+//    Actual: johnny5
+
+// 12. Prediction: 'johnny' - 5 = Nan
+//    Actual: Nan
+
+// 12. Prediction: 99 * "hello" = Nan
+//    Actual: Nan
+
+// 12. Prediction: 1 != 1 = false
+//    Actual: false
+
+// 12. Prediction: 1 != '1' = false
+//    Actual: true
+
+// 12. Prediction: 1 === '1' = false
+//    Actual: false
+
+
+// Exercise 5
+
+// 1. Prediction: 5 + "34" = 534 concatenation
+//    Actual: 534
+
+// 2. Prediction: 5 - '4' = 1
+//    Actual: 1
+
+// 3. Prediction: 5 - '4' = 1
+//    Actual: 1
+
+// 4. Prediction: 10 % 5 = 0.5
+//    Actual: 0
+
+// 5. Prediction: 5 % 10 = ??
+//    Actual: 5
+
+// 6. Prediction: " " + " "
+//    Actual: " "
+
+// 7. Prediction: " " + 0
+//    Actual: 0
+
+// 8. Prediction: true + true = true
+//    Actual: 2
+
+// 9. Prediction: true + false = 1
+//    Actual: 1
+
+// 10. Prediction: false + true = 1
+//    Actual: 1
+
+
+// 11. Prediction: false - true = 0
+//    Actual: 1
+
+// 11. Prediction: !true = false
+//    Actual: false
+
+// 12. Prediction: 3 - 4
+//    Actual: -1
+
+
+console.log(3 - 4)
