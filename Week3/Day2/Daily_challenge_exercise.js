6793e8ecfbf1d5ff25d7b5f56f8fd67d84103bce
Major_ambiton0 <messonvantard@gmail.com>
2025-02-27 13:15:36 +0400

    daily challenge JS arrays & methods

---
+++ Week3/Day2/Daily_challenge_exercise.js
@@ -0,0 +1,21 @@
+// Exercise 1
+
+// const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// fruits.shift("Banana");
+
+// fruits.sort();
+
+// fruits.push("Kiwi");
+
+// fruits.splice(0,1);
+
+// fruits.reverse()
+
+// console.log(fruits)
+
+// Exercise 2
+
+const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
