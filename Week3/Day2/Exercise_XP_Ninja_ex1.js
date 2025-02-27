77a37e6b23fe43b8e913696e72375fe41202cc1c
Major_ambiton0 <messonvantard@gmail.com>
2025-02-27 11:43:52 +0400

    Exercise XP Ninja exercise 3

---
+++ Week3/Day2/Exercise_XP_Ninja_ex1.js
@@ -0,0 +1,33 @@
+// Exercise 1
+
+// 1. Prediction: 5 >=1 = True
+//    Actual: true
+
+// 2. Prediction: 0 === 1 = false
+//    Actual: false
+
+// 3. Prediction: 4 <= 1 = false
+//    Actual: false
+
+// 4. Prediction: 1 != 1
+//    Actual: false
+
+// 5. Prediction: "A" > "B"
+//    Actual: false
+
+// 6. Prediction: "B" < "C"
+//    Actual: true
+
+// 7. Prediction: "a" > "A"
+//    Actual: false
+
+// 8. Prediction: "b" < "A"
+//    Actual: false
+
+// 9. Prediction: true === false
+//    Actual: false
+
+// 10. Prediction: true != true
+//    Actual: false
+
+
