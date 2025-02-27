19a3f078ea0f0ba55b90267de46b7e96ef69b537
Major_ambiton0 <messonvantard@gmail.com>
2025-02-27 10:20:18 +0400

    Exercise XP Gold exercise 1 & 2

---
+++ Week3/Day2/Exercise_XP_Gold_Exercise_1_2.js
@@ -0,0 +1,23 @@
+// Exercise 1
+
+// let sentence = ["my","favorite","color","is","blue"];
+
+// console.log(sentence.join(""))
+
+// Exercise 2
+
+// let str1 = "mix";
+// let str2 = "pod";
+
+// let newStr1 = str2.slice(0,2) + str1.slice(2);
+// let newStr2 = str1.slice(0,2) + str2.slice(2);
+
+// console.log(newStr1)
+// console.log(newStr2)
+
+// let str3 = "cool";
+
+// console.log(str1 + " "+ str2 + " " +str3)
+
+
+
