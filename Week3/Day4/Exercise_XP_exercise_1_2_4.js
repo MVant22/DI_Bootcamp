89fe7cb936cd07bc3f521a92edeec2618dc23982
Major_ambiton0 <messonvantard@gmail.com>
2025-02-27 17:19:38 +0400

    Exercise XP exercise 4

--- Week3/Day4/Exercise_XP_exercise_1_2_4.js
+++ Week3/Day4/Exercise_XP_exercise_1_2_4.js
@@ -29,3 +29,15 @@
 
 const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 
+if (users.length === 0){
+    console.log("no one is online");
+} 
+else if (users.length === 1){
+    console.log(`${users[0]} is online`);
+}
+else if (users.length === 2) {
+    console.log(`${users[0]} and ${users[1]} are online`);
+}
+else {
+    console.log(`${users[0]} and ${users.length - 1} more are online`)
+}
