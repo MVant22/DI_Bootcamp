fb025aa1578ad50edbf36facb3c96172966fffe2
Major_ambiton0 <messonvantard@gmail.com>
2025-02-27 17:12:10 +0400

    Exercise XP 1,2,3

---
+++ Week3/Day4/Exercise_XP_exercise_1_2_4.js
@@ -0,0 +1,31 @@
+// Exercise 1
+
+// let x = 5;
+// let y = 2;
+
+// if (x > y) {
+//     console.log(x + " is greater than " + y)
+// }else {
+//     console.log(y + " is greater than" + x)
+// }
+
+// Exercise 2
+
+// let newDog = 'Chihuahua';
+// // const newDogArary = newDog.split("");
+// // console.log(newDogArary.length)
+
+// // console.log(newDog.toUpperCase());
+// // console.log(newDog.toLowerCase());
+
+// if (newDog == 'Chihuahua') {
+//     console.log("I love " + newDog + " , it`s my favorite dog breed");
+// }else {
+//     console.log("i don`t care, i prefer cats")
+// }
+
+
+// Exercise 4
+
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+
