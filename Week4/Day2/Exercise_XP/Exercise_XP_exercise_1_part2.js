e6176c53375bb6c8c3c5f353b873658c2e6bc549
Major_ambiton0 <messonvantard@gmail.com>
2025-03-03 21:41:52 +0400

    Exercise XP exercise 1 part 2

---
+++ Week4/Day2/Exercise_XP/Exercise_XP_exercise_1_part2.js
@@ -0,0 +1,6 @@
+function infoAboutPerson(personName, personAge, personFavoriteColor){
+    console.log(`My name is ${personName}, i am ${personAge}, my favorite color is ${personFavoriteColor}`);
+}
+
+infoAboutPerson("David", 45, "blue")
+infoAboutPerson("Josh", 12, "yellow")
