e699210a9ead56337e8163e785470f45f527bfca
Major_ambiton0 <messonvantard@gmail.com>
2025-04-15 07:01:08 +0400

    Daily Challenge Advanced Functions & Objects

---
+++ Week8/Daily_Challenge/Daily_challenge.js
@@ -0,0 +1,29 @@
+let client = "John";
+
+const groceries = {
+    fruits : ["pear", "apple", "banana"],
+    vegetables: ["tomatoes", "cucumber", "salad"],
+    totalPrice : "20$",
+    other : {
+        paid : true,
+        meansOfPayment : ["cash", "creditCard"]
+    }
+}
+
+groceries.fruits.forEach(fruit => console.log(fruit))
+
+let cloneGroceries = () => {
+    let user = client;
+    client = "Betty";
+
+    let shopping = groceries;
+    groceries.totalPrice = "35$"
+
+    groceries.other.paid = "false";
+
+    return (user , shopping)
+};
+
+const result = cloneGroceries();
+
+console.log(result)
